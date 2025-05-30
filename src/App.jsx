da0362aea44f18fa0cc0c06f07fb71fbefebfcab
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-05-30 12:13:26 +0530

    Skills and Education Commit

--- src/App.jsx
+++ src/App.jsx
@@ -171,13 +171,13 @@ function App() {
   <section className="w-[750px] mx-auto my-6">
         <h1 className="text-2xl font-bold">Skills</h1>
         <div className="mt-6">
-          <div className="flex items-center gap-4">
-           <p className="bg-black w-18 font-bold text-white text-sm rounded-sm p-2">HTML</p>
-           <p className="bg-black w-18 font-bold text-white text-sm rounded-sm p-2">CSS</p>
-           <p className="bg-black w-18 font-bold text-white text-sm rounded-sm p-2">Bootstrap</p>
-           <p className="bg-black w-18 font-bold text-white text-sm rounded-sm p-2">Tailwind CSS</p>
-           <p className="bg-black w-18 font-bold text-white text-sm rounded-sm p-2">JavaScript</p>
-            <p className="bg-black w-18 font-bold text-white text-sm rounded-sm p-2">React JS</p>
+          <div className="flex items-center gap-4 text-center">
+           <p className="bg-black w-20 font-bold text-white text-sm rounded-sm p-2">HTML</p>
+           <p className="bg-black w-20 font-bold text-white text-sm rounded-sm p-2">CSS</p>
+           <p className="bg-black w-20 font-bold text-white text-sm rounded-sm p-2">Bootstrap</p>
+           <p className="bg-black w-20 font-bold text-white text-sm rounded-sm p-2">Tailwind CSS</p>
+           <p className="bg-black w-20 font-bold text-white text-sm rounded-sm p-2">JavaScript</p>
+            <p className="bg-black w-20 font-bold text-white text-sm rounded-sm p-2">React JS</p>
           </div>
           </div>
           </section>
