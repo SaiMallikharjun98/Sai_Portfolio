b8104f092121f87c6c765e309f6c89121e6836c1
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-05-31 14:23:06 +0530

    Projects Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ function Home() {
           <div>
             <img
               src={User}
-              className="h-34 w-52 rounded-full object-cover"
+              className="h-40 w-60 rounded-full object-cover"
               alt="SelfImage"
             />
           </div>
