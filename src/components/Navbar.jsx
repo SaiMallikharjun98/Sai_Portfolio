dd5d2c711298bce4f4145fbc21355b7ab19403bb
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-06-02 20:51:23 +0530

    Navbar Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <>
       <div className="flex justify-between gap-4 font-bold text-xl w-full mx-auto">
         <h3 className="text-2xl font-bold">SAI</h3>
-        <Link to="/">Home</Link>
+        <Link to="/" className="hover:text-blue-600 cursor-pointer">Home</Link>
         <Link to="/About">About</Link>
         <Link to="/Experience">Experience</Link>
         <Link to="/Qualifications">Qualifications</Link>
