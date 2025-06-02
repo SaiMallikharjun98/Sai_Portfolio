26779f016e71b2ada02c8ee15a49355f6087ed08
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-06-02 20:54:54 +0530

    Navbar styles changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,32 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <>
-      <div className="flex justify-between gap-4 font-bold text-xl w-full mx-auto">
-        <h3 className="text-2xl font-bold">SAI</h3>
-        <Link to="/" className="hover:text-blue-600 cursor-pointer">Home</Link>
-        <Link to="/About">About</Link>
-        <Link to="/Experience">Experience</Link>
-        <Link to="/Qualifications">Qualifications</Link>
-        <Link to="/Skills">Skills</Link>
-        <Link to="/Projects">Projects</Link>
-        <Link to="/Contact">Contact</Link>
+      <div className="flex justify-between gap-3 font-bold text-xl w-[750px] mx-auto">
+        <h3 className="text-2xl font-bold text-blue-500">SAI</h3>
+        <Link to="/" className="hover:text-blue-700 cursor-pointer">
+          Home
+        </Link>
+        <Link to="/About" className="hover:text-blue-700 cursor-pointer">
+          About
+        </Link>
+        <Link to="/Experience" className="hover:text-blue-700 cursor-pointer">
+          Experience
+        </Link>
+        <Link
+          to="/Qualifications"
+          className="hover:text-blue-700 cursor-pointer"
+        >
+          Qualifications
+        </Link>
+        <Link to="/Skills" className="hover:text-blue-700 cursor-pointer">
+          Skills
+        </Link>
+        <Link to="/Projects" className="hover:text-blue-700 cursor-pointer">
+          Projects
+        </Link>
+        <Link to="/Contact" className="hover:text-blue-700 cursor-pointer">
+          Contact
+        </Link>
       </div>
     </>
   );
