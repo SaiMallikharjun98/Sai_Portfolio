e618ba0e6f6f676eef44fe90457433d21a02caaf
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-06-02 21:36:05 +0530

    Final Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,7 +29,7 @@ function Home() {
             />
           </div>
         </div>
-        <div className="flex   mt-5 gap-10">
+        <div className="flex ml-24  mt-5 gap-10">
           <a
             href="https://github.com/SaiMallikharjun98"
             target="_blank"
