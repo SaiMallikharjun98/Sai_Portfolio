d6d58fdb360100422bf92abf90bd0d223d926830
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-06-02 20:51:15 +0530

    Navbar Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <>
-      <div className="flex justify-between gap-4 font-bold text-xl ">
+      <div className="flex justify-between gap-4 font-bold text-xl w-full mx-auto">
         <h3 className="text-2xl font-bold">SAI</h3>
         <Link to="/">Home</Link>
         <Link to="/About">About</Link>
