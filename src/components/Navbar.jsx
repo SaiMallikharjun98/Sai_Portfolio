f7f0ac2d0e16cb0d8a7a3bddccf22fee3b7b75d4
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-06-02 21:06:55 +0530

    Navbar position added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <>
-      <div className="flex justify-between gap-5 font-bold text-xl w-full mt-5 fixed top-0 left-20 shadow-2xl bg-neutral-400">
+      <div className="flex justify-between gap-5 font-bold text-xl w-full  fixed top-0 p-4 shadow-2xl bg-neutral-400">
         <h3 className="text-2xl font-bold text-blue-500 ml-10">SAI</h3>
         <Link to="/" className="hover:text-blue-700 cursor-pointer">
           Home
